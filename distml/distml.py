d8102bb6752c29410cdedadfab6d33b792b7083a
APTG <avilay@terminus-2>
2022-01-19 04:57:53 +0000

    distml wip

--- distml/distml.py
+++ distml/distml.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 import torch.distributed as dist
 from cprint import cprint
@@ -6,5 +7,6 @@ from cprint import cprint
 
 def dprint(text):
     pid = os.getpid()
+    now = datetime.now().strftime("%m-%d %I:%M:%S:%f %p")
     rank = dist.get_rank() if dist.is_available() and dist.is_initialized() else -1
-    cprint(rank, f"[{rank}]({pid}) - {text}")
+    cprint(rank, f"[{rank}]({pid}){now} - {text}")
